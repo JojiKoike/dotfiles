324384cef76da031e91c002b553f1c1d0125ba19
JojiKoike <joji.koike@gmail.com>
2021-12-11 18:01:42 +0900

    Implement Setup Script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-DOT_FILES=(.zshrc .vimrc .tmux.conf .bash_profile)
+# Home直下
+DOT_FILES=(.zshrc .vimrc .vsvimrc .tmux.conf .gitconfig .commit_template .iceberg.itermcolors)
 
 for file in ${DOT_FILES[@]}
 do
-	ln -s $HOME/dotfiles/$file $HOME/$file
+    if [ -f $HOME/$file ]; then
+	rm -f $HOME/$file
+    fi
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+# $HOME/.config配下
+DOT_FILES=(starship.toml)
+
+for file in ${DOT_FILES[@]}
+do
+    if [ -f $HOME/.config/$file ]; then
+	rm -f $HOME/.config/$file
+    fi
+    ln -s $HOME/dotfiles/.$file $HOME/.config/$file
 done
